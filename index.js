4e01b0139053987af11def34169c91a1746d8ac0
hieung.tom <hieung.tom@gmail.com>
2025-12-03 02:19:08 +0100

    css changes, fully responsive contact html, added content to home page

--- index.js
+++ index.js
@@ -39,6 +39,30 @@ searchForm.addEventListener("submit", function (event) {
   }
 });
 
+//prepinani kontaktu
+const keySelect = document.getElementById("key-select");
+const departmentSelect = document.getElementById("department-select");
+const keyContactsGroup = document.getElementById("key-contacts-group");
+const departmentContactsGroup = document.getElementById(
+  "department-contacts-group"
+);
+
+if (departmentSelect) {
+  departmentSelect.addEventListener("click", function () {
+    keyContactsGroup.classList.add("hidden-group");
+    departmentContactsGroup.classList.remove("hidden-group");
+    keySelect.classList.remove("active-select");
+    departmentSelect.classList.add("active-select");
+  });
+
+  keySelect.addEventListener("click", function () {
+    keyContactsGroup.classList.remove("hidden-group");
+    departmentContactsGroup.classList.add("hidden-group");
+    departmentSelect.classList.remove("active-select");
+    keySelect.classList.add("active-select");
+  });
+}
+
 //render kalendare
 const currentPage = window.location.pathname.split("/").pop();
 let departmentSchedules = {
